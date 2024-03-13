bb5c88713d81610374d41bce058b5510ccb4b351
Flavia Ballabene <139667150+FlaviaBallabene@users.noreply.github.com>
2024-03-13 00:03:55 +0000

    project completed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,53 +4,59 @@ const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
 
-
   useEffect(() => {
-    const fetchForPosts = async () => {
-    try {
-      const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/flaviaballab-todolist");
-      const posts = await response.json();
-      setTodos(posts);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
     fetchForPosts();
   }, []);
 
-  const postData = async (data) => {
+  const fetchForPosts = async () => {
     try {
-      const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/flaviaballab-todolist", {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      setTodos(data);
+      const response = await fetch(
+        "https://playground.4geeks.com/apis/fake/todos/user/flaviaballab-todolist"
+      );
+      const posts = await response.json();
+      setTodos(posts);
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
   };
 
-  const deleteData = async (index) => {
+  const updateData = async (data) => {
     try {
-      const response = await fetch(`https://playground.4geeks.com/apis/fake/todos/user/flaviaballab-todolist/${index}`, {
-        method: 'DELETE'
-      });
-      if (!response.ok) {
-        throw new Error(response.statusText);
+      const response = await fetch(
+        "https://playground.4geeks.com/apis/fake/todos/user/flaviaballab-todolist",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      if (response.ok) {
+        fetchForPosts();
       }
-      const updatedTodos = todos.filter((_, i) => i !== index);
-      setTodos(updatedTodos);
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
   };
+
+  // const deleteData = async (index) => {
+  //   const updatedTodos = todos.filter((_, i) => i !== index);
+  //   try {
+  //     const response = await fetch(`https://playground.4geeks.com/apis/fake/todos/user/flaviaballab-todolist`, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify(updatedTodos)
+  //     });
+  //     if (response.ok){
+  //       fetchForPosts();
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
     <div className="text-center">
@@ -67,30 +73,26 @@ const Home = () => {
               if (e.key === "Enter" && inputValue.trim() !== "") {
                 setTodos(todos.concat(inputValue));
                 setInputValue("");
-                const newData =  [
-                  ...todos,
-                  { label: inputValue, done: false }
-                  ];
-                            postData(newData);
+                const newData = [...todos, { label: inputValue, done: false }];
+                updateData(newData);
               }
             }}
           ></input>
         </li>
         {todos.map((item, index) => (
-          <li className="list-group-item">
+          <li key={index} className="list-group-item">
             {item.label}
             <svg
               onClick={() => {
-                deleteData(index);
-                setTodos(
-                  todos.filter((t, currentIndex) => index !== currentIndex)
-                );
+                // deleteData(index);
+                const updatedTodos = todos.filter((_, i) => i !== index);
+                updateData(updatedTodos);
               }}
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               fill="currentColor"
-              class="bi bi-trash3"
+              className="bi bi-trash3"
               viewBox="0 0 16 16"
             >
               <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
